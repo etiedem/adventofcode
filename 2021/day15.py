51b4565a8fb34e8a897d1c3de28a1c2d127218a5
Eric Tiedemann <{ID}+{username}@users.noreply.github.com>
2021-12-26 11:27:14 -0500

    2021 Day15

--- 2021/day15.py
+++ 2021/day15.py
@@ -1,8 +1,84 @@
 import heapq
 from collections import defaultdict
+from copy import deepcopy
 from dataclasses import dataclass, field
 
 from rich import print
+from rich.console import Console
+from rich.text import Text
+
+
+@dataclass(slots=True)
+class Node:
+    x: int
+    y: int
+    cost: int
+    f: int = float('inf')
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+@dataclass(slots=True)
+class Graph:
+    _base: list[list[int]]
+    graph: list[list[Node]] = field(default_factory=list)
+
+    def __post_init__(self):
+        self.graph = [[Node(x, y, cost)
+                       for x, cost in enumerate(row)]
+                      for y, row in enumerate(self._base)]
+        self._base = []
+
+    def get_neighbors(self, node: Node):
+        return [
+            self.graph[ny][nx]
+            for x, y in ((1, 0), (0, 1), (-1, 0), (0, -1))
+            if (nx := node.x + x) < len(self.graph[0]) and nx >= 0 and
+            (ny := node.y + y) < len(self.graph) and ny >= 0
+        ]
+
+    def print(self, path=None):
+        console = Console()
+
+        path = [] if path is None else path
+        for row in self.graph:
+            for item in row:
+                if item in path:
+                    console.print(Text(f'{item.cost:>1d}', style='bold white'), end='')
+                else:
+                    console.print(
+                        Text(f'{item.cost:>1d}', style='dark_turquoise'), end=''
+                    )
+            print()
+
+    def expand(self, num):
+        x_len = len(self.graph[0])
+        y_len = len(self.graph)
+
+        # Copy Original
+        result = deepcopy(self.graph)
+
+        # Expand X
+        for n in range(num - 1):
+            for y, row in enumerate(self.graph):
+                result[y].extend([
+                    Node(x + x_len * (n+1), y, ((item.cost + n) % 9) + 1)
+                    for x, item in enumerate(row)
+                ])
+
+        self.graph = deepcopy(result)
+
+        # Expand Y
+        for n in range(num - 1):
+            for y, row in enumerate(result):
+                self.graph.append([
+                    Node(x, y + y_len * (n+1), ((item.cost + n) % 9) + 1)
+                    for x, item in enumerate(row)
+                ])
 
 
 def get_data(filename):
@@ -10,81 +86,54 @@ def get_data(filename):
         return [[int(l) for l in line.strip()] for line in f.readlines()]
 
 
-# class Node:
-
-#     def __init__(self, x, y, cost):
-#         self.x = x
-#         self.y = y
-#         self.cost = cost
-
-#     def __lt__(self, other):
-#         return self.cost < other.cost
-
-#     def __eq__(self, other):
-#         return (self.x, self.y) == (other.x, other.y)
-
-#     def __hash__(self):
-#         return hash(repr(self))
-
-#     def __repr__(self):
-#         return f'{self.__class__.__name__}(x={self.x}, y={self.y}, cost={self.cost})'
-
-# @dataclass(eq=True)
-# class Node:
-#     x: int
-#     y: int
-#     cost: int
-#     g: int = 1 << 20
-#     f: int = 1 << 20
-#     parent: int = None
-
-#     def __lt__(self, other):
-#         return self.f < other.f
+def manhattan_distance(n1, n2):
+    return abs(n1.x - n2.x) + abs(n1.y - n2.y)
 
 
-def euclidean_distance(n1, n2):
-    return abs(n1.x - n2.y) + abs(n1.y - n2.y)
+def A_star(graph: Graph, source: Node, target: Node):
+    open_set = [source]
+    previous = {}
+    g_score = defaultdict(lambda: float('inf'))
+    g_score[source] = 0
+    source.f = manhattan_distance(source, target)
+
+    while open_set:
+        current = heapq.heappop(open_set)
+
+        if current == target:
+            return g_score, previous
+
+        for neighbor in graph.get_neighbors(current):
+            alt = g_score[current] + neighbor.cost
+            if alt < g_score[neighbor]:
+                previous[neighbor] = current
+                g_score[neighbor] = alt
+                neighbor.f = alt + manhattan_distance(neighbor, target)
+                if neighbor not in open_set:
+                    heapq.heappush(open_set, neighbor)
 
 
-def Dijkstra(graph, source, target):
-    Q = [(0, source[1], source[2])]
-    visited = set()
-    cost = defaultdict(int)
-
-    while Q:
-        current = heapq.heappop(Q)
-        if current in visited:
-            continue
-        visited.add(current)
-        cost[(current[1], current[2])] = current[0]
-
-        if (current[1], current[2]) == (target[1], target[2]):
-            break
-
-        for neighbor in get_neighbors(current, graph):
-            heapq.heappush(Q, (neighbor[0] + current[0], neighbor[1], neighbor[2]))
-
-    return cost[target[1], target[2]]
-
-
-def get_neighbors(node, graph):
-    return [
-        graph[node[2] + dy][node[1] + dx]
-        for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0))
-        if node[1] + dx < len(graph[0]) and node[1] + dx >= 0 and node[2] +
-        dy < len(graph) and node[2] + dy >= 0
-    ]
-
-
-def create_map(data):
-    return [[(cost, x, y) for x, cost in enumerate(row)] for y, row in enumerate(data)]
+def get_path(prev, source, target):
+    path = []
+    answer = 0
+    if prev[target] is not None:
+        while target and target != source:
+            path.append(target)
+            answer += target.cost
+            target = prev[target]
+    return path, answer
 
 
 def main():
     data = get_data('day15.txt')
-    node_map = create_map(data)
-    path = Dijkstra(node_map, node_map[0][0], node_map[9][9])
-    print(path)
+    graph = Graph(data)
+    graph.expand(5)
+    source = graph.graph[0][0]
+    target = graph.graph[-1][-1]
+    dist, prev = A_star(graph, source, target)
+    path, answer = get_path(prev, source, target)
+    # graph.print(path)
+    print(answer)
 
 
 if __name__ == "__main__":
--- 2021/day15_dijkstra.md
+++ 2021/day15_dijkstra.md
@@ -25,3 +25,30 @@ function Dijkstra(Graph, source):
 
   return dist[], prev[]
 ```
+
+## Pseudocode with Priority
+```python
+function Dijkstra(Graph, source):
+  dist[source] ← 0                           // Initialization
+
+  create vertex priority queue Q
+
+  for each vertex v in Graph:
+      if v ≠ source
+          dist[v] ← INFINITY                 // Unknown distance from source to v
+          prev[v] ← UNDEFINED                // Predecessor of v
+
+      Q.add_with_priority(v, dist[v])
+
+
+  while Q is not empty:                      // The main loop
+      u ← Q.extract_min()                    // Remove and return best vertex
+      for each neighbor v of u:              // only v that are still in Q
+          alt ← dist[u] + length(u, v)
+          if alt < dist[v]
+              dist[v] ← alt
+              prev[v] ← u
+              Q.decrease_priority(v, alt)
+
+  return dist, prev
+```
