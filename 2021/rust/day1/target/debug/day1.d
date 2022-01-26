cadc8794299a57de4918d0a228f9fc5bd6ba2fcc
Eric Tiedemann <{ID}+{username}@users.noreply.github.com>
2022-01-26 09:38:59 -0500

    remove target folder

--- 2021/rust/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-D:\Advent\ of\ Code\2021\rust\day1\target\debug\day1.exe: D:\Advent\ of\ Code\2021\rust\day1\src\main.rs
--- 2021/rust/day1/target/debug/deps/day1.d
+++
@@ -1,5 +0,0 @@
-D:\Advent of Code\2021\rust\day1\target\debug\deps\day1.exe: src\main.rs
-
-D:\Advent of Code\2021\rust\day1\target\debug\deps\day1.d: src\main.rs
-
-src\main.rs:
