8fb020c5a749d36b2d040f3d4d37a9de92b37d59
Eric Tiedemann <{ID}+{username}@users.noreply.github.com>
2022-12-02 11:56:17 -0500

    day1 complete

---
+++ 2022/python/day1.py
@@ -0,0 +1,32 @@
+from functools import partial
+from itertools import chain
+
+from rich import print
+
+
+def get_data(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        for x in f:
+            yield x.strip()
+
+
+def largest(data, num):
+    return sum(sum(x) for x in sorted(data, key=sum, reverse=True)[:num])
+
+
+def main():
+    elf = []
+    tmp = []
+    for calorie in get_data(r"day1.txt"):
+        if calorie:
+            tmp.append(int(calorie))
+        else:
+            elf.append(tmp)
+            tmp = []
+
+    print(f"PART1: {largest(elf, 1)}")
+    print(f"PART2: {largest(elf, 3)}")
+
+
+if __name__ == "__main__":
+    main()
