e36847d6017144932a9e34ea114f373b0600533f
Eric Tiedemann <{ID}+{username}@users.noreply.github.com>
2022-12-04 11:53:35 -0500

    day4 part1

--- 2022/rust/day4/src/main.rs
+++ 2022/rust/day4/src/main.rs
@@ -1,3 +1,52 @@
+use std::fs::File;
+use std::io::Read;
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Interval {
+    start: u32,
+    end: u32,
+}
+
+fn contains(slf: &Interval, other: &Interval) -> bool {
+    other.start >= slf.start && other.end <= slf.end
+}
+
+fn get_data(filename: &str) -> String {
+    let mut file = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("something went wrong reading the file");
+    contents
+}
+
+fn parse_data(data: String) -> Vec<Vec<Interval>>  {
+
+    data.split_whitespace()
+        .map(|line|
+            line.split(',').
+                map(|pair|
+                    {
+                        let mut iter = pair.split('-');
+                        let start = iter.next().unwrap().parse::<u32>().unwrap();
+                        let end = iter.next().unwrap().parse::<u32>().unwrap();
+                        Interval{start, end}
+                    }
+                ).collect()
+        ).collect()
+}
+
+
+
+fn part1(data: &Vec<Vec<Interval>>) -> u32 {
+    let mut count = 0;
+    for pair in data {
+        let (a, b) = (&pair[0], &pair[1]);
+        if contains(a, b) || contains(b, a) {
+            count += 1;
+        }
+    }
+    count
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = parse_data(get_data("data.txt"));
+    println!("PART1: {:?}", part1(&data));
 }
