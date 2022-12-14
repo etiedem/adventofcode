da2346c45681113448e2b59a9ddc2a8f8ef917c0
Eric Tiedemann <etiedem01@users.noreply.github.com>
2022-12-14 15:13:55 -0500

    Removed Head tracking and moved logic into the segments

--- 2022/rust/day9/src/main.rs
+++ 2022/rust/day9/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::HashSet,
-    ops::{AddAssign, Sub, SubAssign},
+    ops::{AddAssign, Sub, SubAssign, Add},
 };
 
 fn parse_data(data: &str) -> Vec<Vec<&str>> {
@@ -22,6 +22,12 @@ impl SubAssign for Coord {
         *self = Self(self.0 - other.0, self.1 - other.1)
     }
 }
+impl Add for &Coord {
+    type Output = Coord;
+    fn add(self, other: &Coord) -> Self::Output {
+        Coord(self.0 + other.0, self.1 + other.1)
+    }
+}
 impl Sub for &Coord {
     type Output = Coord;
     fn sub(self, other: &Coord) -> Self::Output {
@@ -29,74 +35,45 @@ impl Sub for &Coord {
     }
 }
 
-// fn move_segment(head: &Coord, tail: &mut Coord) -> HashSet<Coord> {
-//     let mut map = HashSet::new();
-//     let mut diff = &head.clone() - &tail.clone();
-//     if (diff.0.abs() > 1 && diff.1.abs() != 0) || (diff.1.abs() > 1 && diff.0.abs() != 0) {
-//         let x = if diff.0 > 0 { 1 } else { -1 };
-//         let y = if diff.1 > 0 { 1 } else { -1 };
-//         *tail = Coord(tail.0 + x, tail.1 + y);
-//         diff = &head.clone() - &tail.clone();
-//         map.insert(*tail);
-//     }
-//     if diff.0.abs() > 1 {
-//         let x = if diff.0 > 0 { 1 } else { -1 };
-
-//         for _ in 0..diff.0.abs() - 1 {
-//             *tail = Coord(tail.0 + x, tail.1);
-//             map.insert(*tail);
-//         }
-//     }
-//     if diff.1.abs() > 1 {
-//         let y = if diff.1 > 0 { 1 } else { -1 };
-
-//         for _ in 0..diff.1.abs() - 1 {
-//             *tail = Coord(tail.0, tail.1 + y);
-//             map.insert(*tail);
-//         }
-//     }
-//     map
-// }
-
-fn move_segment(head: &Coord, tail: &mut Coord) -> HashSet<Coord> {
+fn move_segment(prev: &Coord, cur: &mut Coord, head: bool) -> HashSet<Coord> {
     let mut map = HashSet::new();
-    let mut diff = &head.clone() - &tail.clone();
-    while diff.0.abs() > 1 || diff.1.abs() > 1 {
+    let mut diff = &prev.clone() - &cur.clone();
+    let distance = if head { 0 } else { 1 };
+    while diff.0.abs() > distance || diff.1.abs() > distance {
         let x = if diff.0 > 0 { 1 } else if diff.0 < 0 { -1 } else { 0 };
         let y = if diff.1 > 0 { 1 } else if diff.1 < 0 { -1 } else { 0 };
-        *tail = Coord(tail.0 + x, tail.1 + y);
-        diff = &head.clone() - &tail.clone();
-        map.insert(*tail);
+        *cur = Coord(cur.0 + x, cur.1 + y);
+        diff = &prev.clone() - &cur.clone();
+        map.insert(*cur);
     }
     map
 }
 
-fn move_all(head: &Coord, segments: &mut Vec<Coord>) -> HashSet<Coord> {
+fn move_all(end: Coord, segments: &mut Vec<Coord>) -> HashSet<Coord> {
     let mut map = HashSet::new();
-    let mut head = head.clone();
+    let mut prev = end;
     let mut new_segments = Vec::new();
 
-    for mut segment in segments {
-        map = move_segment(&head, &mut segment);
-        head = *segment;
+    for (count, mut segment) in (0..segments.len()).zip(segments) {
+        map = move_segment(&prev, &mut segment, count==0);
+        prev = *segment;
         new_segments.push(*segment);
     }
     map
 }
 
 fn run(moves: &Vec<Vec<&str>>, num_seg: u32) -> u32 {
-    let mut head = Coord(0, 0);
-    let mut segments = vec![Coord(0, 0); num_seg as usize - 1];
+    let mut segments = vec![Coord(0, 0); num_seg as usize];
     let mut seen: HashSet<Coord> = HashSet::new();
     seen.insert(Coord(0, 0));
 
     for m in moves {
         let (dir, steps) = (m[0], m[1].parse::<i32>().unwrap());
         let update = match dir {
-            "R" => { head += Coord(steps, 0); move_all(&head, &mut segments) }
-            "L" => { head += Coord(-steps,0); move_all(&head, &mut segments) },
-            "U" => { head += Coord(0,steps);  move_all(&head, &mut segments) },
-            "D" => { head += Coord(0,-steps); move_all(&head, &mut segments) },
+            "R" => { let end = &segments[0] + &Coord(steps, 0); move_all(end, &mut segments) }
+            "L" => { let end = &segments[0] + &Coord(-steps,0); move_all(end, &mut segments) },
+            "U" => { let end = &segments[0] + &Coord(0,steps);  move_all(end, &mut segments) },
+            "D" => { let end = &segments[0] + &Coord(0,-steps); move_all(end, &mut segments) },
             _ => panic!("Unknown Movement"),
         };
         seen.extend(&update);
