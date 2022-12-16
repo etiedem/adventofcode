1b2d2a257de40339ff97c7e4e6bf33d7059243da
Eric Tiedemann <etiedem01@users.noreply.github.com>
2022-12-16 11:59:22 -0500

    day10 complete

--- 2022/rust/day10/src/main.rs
+++ 2022/rust/day10/src/main.rs
@@ -18,29 +18,40 @@ fn parse_data(data: &str) -> Vec<Command> {
         }).collect()
 }
 
-fn execute(commands: &Vec<Command>) -> Vec<i32> {
+fn run(commands: &Vec<Command>) -> (Vec<i32>, Vec<&str>) {
     let mut x = 1;
     let mut cycle = 0;
-    let mut register = Vec::new();
+    let mut signal_strength = Vec::new();
+    let mut image = Vec::new();
 
     let mut get = 20;
     for command in commands {
         for c in 0..command.cycles {
+            if [x-1,x,x+1].contains(&(cycle%40)) {
+                image.push("#");
+            } else {
+                image.push(" ");
+            }
             cycle += 1;
             if cycle == get {
                 get += 40;
-                register.push(x * cycle);
+                signal_strength.push(x * cycle);
             }
             if c == command.cycles-1 {
                 x += command.number;
             }
         }
     }
-    register
+    (signal_strength, image)
 }
 
 fn main() {
     let data = parse_data(include_str!("data.txt"));
-    println!("Part 1: {:?}", execute(&data).iter().sum::<i32>());
+    let (signal_strength, image) = run(&data);
+    println!("Part 1: {:?}", signal_strength.iter().sum::<i32>());
+    println!("Part 2:");
+    for chunk in image.chunks(40) {
+        println!("{}", chunk.join(""));
+    }
 }
 
