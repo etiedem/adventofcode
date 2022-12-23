5c099d0bd927254aa30bdb43ac3634a3736f56c7
Eric Tiedemann <etiedem01@users.noreply.github.com>
2022-12-23 10:10:37 -0500

    Cleanup day1

--- 2022/rust/day1/src/main.rs
+++ 2022/rust/day1/src/main.rs
@@ -1,35 +1,21 @@
-use std::fs::File;
-use std::io::Read;
-use std::cmp::Reverse;
-
-fn get_data(filename: &str) -> String {
-    let mut file = File::open(filename).expect("file not found");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("something went wrong reading the file");
-    contents
+fn parse_data(data: &str) -> Vec<Vec<u32>> {
+    data.split("\n\n")
+        .map(|section| {
+            section
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect()
+        })
+        .collect()
 }
 
-fn parse_data(data: String) -> Vec<Vec<u32>> {
-    let mut result = Vec::new();
-    let mut tmp = Vec::new();
-    for line in data.lines() {
-        if !line.is_empty() {
-           tmp.push(line.parse::<u32>().unwrap());
-        } else {
-            result.push(tmp);
-            tmp = Vec::new();
-        }
-    }
-    result
-}
-
-fn largest(data: &Vec<Vec<u32>>, num: usize) -> u32 {
-    let mut d = data.clone();
-    d.sort_by_key(|x| Reverse(x.iter().sum::<u32>()));
-    d[..num].to_vec().iter().map(|x| x.iter().sum::<u32>()).sum::<u32>()
+fn largest(data: &[Vec<u32>], num: usize) -> u32 {
+    let mut calories: Vec<u32> = data.iter().map(|x| x.iter().sum()).collect();
+    calories.sort_by(|a, b| b.cmp(a));
+    calories.iter().take(num).sum()
 }
 fn main() {
-    let data = parse_data(get_data("data.txt"));
+    let data = parse_data(include_str!("data.txt"));
     println!("PART1: {:?}", largest(&data, 1));
     println!("PART2: {:?}", largest(&data, 3));
 }
