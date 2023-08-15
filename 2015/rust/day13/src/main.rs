3a662ceadbd39457aaa6c39870a2bf8aff3919e0
Eric Tiedemann <etiedem01@users.noreply.github.com>
2023-08-15 10:15:41 -0400

    2015 day 13 - fix clippy

--- 2015/rust/day13/src/main.rs
+++ 2015/rust/day13/src/main.rs
@@ -6,7 +6,7 @@ fn create_keys(d: &str) -> (HashMap<String, usize>, HashMap<String, usize>) {
     for line in d.lines() {
         let words: Vec<_> = line.split_whitespace().collect();
         keys.insert(words[0].to_owned());
-        keys.insert(words[words.len() - 1].trim_end_matches(".").to_owned());
+        keys.insert(words[words.len() - 1].trim_end_matches('.').to_owned());
     }
     let p1_keys: HashMap<String, usize> = keys
         .iter()
@@ -27,7 +27,7 @@ fn create_adjacency_matrix(data: &str, keys: HashMap<String, usize>) -> Vec<Vec<
     for line in data.lines() {
         let words: Vec<_> = line.split_whitespace().collect();
         let from = keys[words[0]];
-        let to = keys[words[words.len() - 1].trim_end_matches(".")];
+        let to = keys[words[words.len() - 1].trim_end_matches('.')];
         let gain_loss = words[2];
         let happy = words[3].parse::<i32>().unwrap();
         match gain_loss {
@@ -50,7 +50,7 @@ fn solve(matrix: Vec<Vec<i32>>, options: Vec<&usize>) -> i32 {
     short
 }
 
-fn score(matrix: &Vec<Vec<i32>>, path: Vec<&&usize>) -> i32 {
+fn score(matrix: &[Vec<i32>], path: Vec<&&usize>) -> i32 {
     let mut total = 0;
     for i in 0..path.len() - 1 {
         total += matrix[**path[i]][**path[i + 1]];
