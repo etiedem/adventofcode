f9f4f7049670628c3dd16ea887a3f86e667fea5d
Eric Tiedemann <etiedem01@users.noreply.github.com>
2024-03-05 14:40:57 -0500

    simplify

--- 2015/go/day07/main.go
+++ 2015/go/day07/main.go
@@ -14,41 +14,27 @@ type Action struct {
 	dest   string
 }
 
-func ParseAction(line string) (string, Action) {
+func ParseAction(line string) *Action {
 	items := strings.Split(line, " ")
 	if len(items) == 3 {
-		action := Action{action: "ASSIGN", left: items[0], dest: items[2]}
-		return action.dest, action
+		return &Action{action: "ASSIGN", left: items[0], dest: items[2]}
 	}
 	if len(items) == 4 {
-		action := Action{action: "NOT", left: items[1], dest: items[3]}
-		return action.dest, action
+		return &Action{action: "NOT", left: items[1], dest: items[3]}
 	}
 	if items[1] == "AND" {
-		action := Action{action: "AND", left: items[0], right: items[2], dest: items[4]}
-		return action.dest, action
+		return &Action{action: "AND", left: items[0], right: items[2], dest: items[4]}
 	}
 	if items[1] == "OR" {
-		action := Action{action: "OR", left: items[0], right: items[2], dest: items[4]}
-		return action.dest, action
+		return &Action{action: "OR", left: items[0], right: items[2], dest: items[4]}
 	}
 	if items[1] == "LSHIFT" {
-		action := Action{action: "LSHIFT", left: items[0], right: items[2], dest: items[4]}
-		return action.dest, action
+		return &Action{action: "LSHIFT", left: items[0], right: items[2], dest: items[4]}
 	}
 	if items[1] == "RSHIFT" {
-		action := Action{action: "RSHIFT", left: items[0], right: items[2], dest: items[4]}
-		return action.dest, action
+		return &Action{action: "RSHIFT", left: items[0], right: items[2], dest: items[4]}
 	}
-	return "", Action{}
-}
-
-func getData(filename string) []byte {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
-	return data
+	return &Action{}
 }
 
 func getDataByLine(filename string) []string {
@@ -65,8 +51,8 @@ func parseData(data []string) map[string]Action {
 		if line == "" {
 			continue
 		}
-		dest, action := ParseAction(line)
-		output[dest] = action
+		action := ParseAction(line)
+		output[(*action).dest] = *action
 	}
 	return output
 }
@@ -154,21 +140,17 @@ func solve(action Action, instr *map[string]Action, output *map[string]uint16) {
 	}
 }
 
-func part1(instr map[string]Action, output *map[string]uint16) uint16 {
-	solve(instr["a"], &instr, output)
-	return (*output)["a"]
-}
-
 func main() {
 	data := getDataByLine("day07.txt")
 	instr := parseData(data)
 
 	output := map[string]uint16{}
-	p1 := part1(instr, &output)
+	solve(instr["a"], &instr, &output)
+	p1 := output["a"]
 	fmt.Println("Part 1:", p1)
 
 	output = map[string]uint16{"b": p1}
-	p2 := part1(instr, &output)
+	solve(instr["a"], &instr, &output)
+	p2 := output["a"]
 	fmt.Println("Part 2:", p2)
 }
-
